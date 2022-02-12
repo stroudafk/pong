11013e5cf186a3f1c34dd47a574ede8930425d07
sj <stroudafk@gmail.com>
2022-02-12 14:48:55 -0600

    random start direction (but not random vector)

--- pong.py
+++ pong.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 
 def ball_animation():
     global ball_speed_x, ball_speed_y
@@ -8,10 +8,11 @@ def ball_animation():
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
     if ball.left <= 0 or ball.right >= screen_width:
-        ball_speed_x *= -1    
-    
+        ball_reset()
+
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
+
 def player_animation():
     player.y += player_speed
     if player.top <= 0:
@@ -19,6 +20,21 @@ def player_animation():
     if player.bottom >= screen_height:
         player.bottom = screen_height
 
+def opponent_ai():
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+    if opponent.top <= 0:
+        opponent.top <= 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
+
+def ball_reset():
+    global ball_speed_x, ball_speed_y
+    ball.center = (screen_width/2, screen_height/2)
+    ball_speed_y *= random.choice((1,-1))
+    ball_speed_x *= random.choice((1,-1))
 # General setup
 pygame.init()
 clock = pygame.time.Clock()
@@ -37,8 +53,8 @@ opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 bg_color = pygame.Color('grey12')
 light_grey = (200, 200, 200)
 
-ball_speed_x = 7
-ball_speed_y = 7
+ball_speed_x = 7 * random.choice((1,-1))
+ball_speed_y = 7 * random.choice((1,-1))
 player_speed = 0
 opponent_speed = 7
 
@@ -61,16 +77,7 @@ while True:
     
     ball_animation()
     player_animation()
-    if opponent.top < ball.y:
-        opponent.top += opponent_speed
-    if opponent.bottom > ball.y:
-        opponent.bottom -= opponent_speed
-    if opponent.top <= 0:
-        opponent.top <= 0
-    if opponent.bottom >= screen_height:
-        opponent.bottom = screen_height
-
-    
+    opponent_ai()    
 
     # Visuals
     screen.fill(bg_color)
