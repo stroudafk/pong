26921f6be595b3bd5487710df5a150e6d40dabe3
sj <stroudafk@gmail.com>
2022-02-12 14:27:10 -0600

    added player movement + animation, boundaries for player and moved ball animation to ball_animation function

--- pong.py
+++ pong.py
@@ -1,5 +1,24 @@
 import pygame, sys
 
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+    
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1    
+    
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+def player_animation():
+    player.y += player_speed
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
+
 # General setup
 pygame.init()
 clock = pygame.time.Clock()
@@ -20,6 +39,7 @@ light_grey = (200, 200, 200)
 
 ball_speed_x = 7
 ball_speed_y = 7
+player_speed = 0
     
 while True:
 	#Handling input
@@ -27,19 +47,19 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player_speed += 7
+            if event.key == pygame.K_UP:
+                player_speed -= 7
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                player_speed -= 7
+            if event.key == pygame.K_UP:
+                player_speed += 7
     
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
-    
-    if ball.top <= 0 or ball.bottom >= screen_height:
-        ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= screen_width:
-        ball_speed_x *= -1    
-    
-    if ball.colliderect(player) or ball.colliderect(opponent):
-        ball_speed_x *= -1
-
-
+    ball_animation()
+    player_animation()
     # Visuals
     screen.fill(bg_color)
     pygame.draw.rect(screen,light_grey, player)
